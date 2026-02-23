77d7b2e84b608953056a43c132a43e179ae4e700
mahmoodalasad <mahmoodalasaad13@gmail.com>
2026-02-23 01:16:57 +0100

    get tasks tool

--- src/main/java/com/example/saasTemplet/mcp/WeatherService.java
+++ src/main/java/com/example/saasTemplet/mcp/WeatherService.java
@@ -3,12 +3,35 @@ package com.example.saasTemplet.mcp;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Service;
 
+import com.example.saasTemplet.task.Task;
+import com.example.saasTemplet.task.TaskRepository;
+
+import java.util.List;
+
 @Service
+
 public class WeatherService {
+
+    private final TaskRepository taskRepository;
+
+    public WeatherService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     @Tool(description = "Get weather info by city name")
     public String getWeather(String city) {
         // your implementation
         return "Weather for " + city + " is sunny";
+    }
+
+    @Tool(description = "Get supported cities")
+    public List<String> getSupportedCities() {
+        // your implementation
+        return List.of("New York", "London", "Tokyo", "Moers");
+    }
+
+    @Tool(description = "get all tasks by user name")
+    public List<Task> getTasks() {
+        return taskRepository.findAll();
     }
 }
